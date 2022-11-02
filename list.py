bfb5699b25a2638ab3b2c7cb1c785f23286ce1ed
Seray1 <obotseteyemfon@gmail.com>
2022-11-02 15:18:49 +0100

    update

---
+++ list.py
@@ -0,0 +1,15 @@
+names = ["seteye", "raymond", "obot"]
+
+print(names)
+
+# list has a number of built in methods,come is .append() which is used to add data to the list
+
+names.append("mkpouto")
+print(names)
+
+#another method is .sort() which sorts the list.
+
+names.sort()
+print(names)
+
+print(names[0])
---
+++ sequences.py
@@ -0,0 +1,22 @@
+# we can fetch data by their appearance using [], eg the code below will return S
+# name = "seteye"
+
+# print(name[0])
+
+# we have different types of sequences in python, one is List which is synonymous with arrays in other languages
+# and is mutable
+
+# cars = ["benz", "toyota", "lambo"]
+
+# print(cars)
+
+# another type of sequence in python is a tuple,we use ( ) to write a tuple and tuples do not change in value meaning they
+# immutable
+
+# point = (10, 23)
+
+# another type of data type is set which is a collection of unique value, it is worthy to note that while list and tuple are orderly,
+# set is not
+
+
+
